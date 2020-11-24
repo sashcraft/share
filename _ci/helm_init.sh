445a6c5eb59f5023164885043789660e492c7cab
Piero Cittadino <63122825+pieCit87@users.noreply.github.com>
2020-11-24 23:41:30 +0200

    restored helm tiller

--- _ci/helm_init.sh
+++ _ci/helm_init.sh
@@ -1,1 +1,1 @@
-helm init --service-account tiller --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | sed 's@  replicas: 1@  replicas: 1\n  selector: {"matchLabels": {"app": "helm", "name": "tiller"}}@' | kubectl apply -f -
+helm init -debug
