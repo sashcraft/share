0e2ed060fa8f42f5089f95798ec7af98d42f4885
Gheorghiu Flavian <gheorghiu.flavian@yahoo.com>
2020-12-10 14:25:21 +0200

    APPS-123: fix license header information

--- share/src/main/java/org/alfresco/web/site/servlet/config/AIMSConfig.java
+++ share/src/main/java/org/alfresco/web/site/servlet/config/AIMSConfig.java
@@ -1,9 +1,13 @@
 /*
- * Copyright 2005 - 2020 Alfresco Software Limited.
- *
+ * #%L
+ * Alfresco Share WAR
+ * %%
+ * Copyright (C) 2005 - 2016 Alfresco Software Limited
+ * %%
  * This file is part of the Alfresco software.
- * If the software was purchased under a paid Alfresco license, the terms of the paid license agreement will prevail.
- * Otherwise, the software is provided under the following open source license terms:
+ * If the software was purchased under a paid Alfresco license, the terms of
+ * the paid license agreement will prevail.  Otherwise, the software is
+ * provided under the following open source license terms:
  *
  * Alfresco is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -17,6 +21,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ * #L%
  */
 package org.alfresco.web.site.servlet.config;
 
