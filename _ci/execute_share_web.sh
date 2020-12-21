b76a1aa8a9b68509e5154fe7e2ff8b7ca7d3e95a
araschitor <andrei.raschitor@ness.com>
2020-12-21 19:42:11 +0200

    feature/APPS-parallel-execution2: set xport 99

--- _ci/execute_share_web.sh
+++ _ci/execute_share_web.sh
@@ -32,6 +32,7 @@ mvn install \
                -Dadmin.user=admin \
                -Dadmin.password=$ALF_PASSWORD \
                -Dbrowser.headless=true \
+               -Ddisplay.xport=99.0 \
                -Dwebdriver.grid.url='http://127.0.0.1:4444/wd/hub' \
                -Daims.enabled=false & # send the long living command to background!
 
